1806f2ee293adf23fc94eb0aeb60c3d5700f0cae
Alex Robinson <alexrcapgemini@gmail.com>
2016-12-07 18:51:48 +0000

    get_data bug fixes

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,14 @@
 # Header information
 get_data <- function(){
         if (!dir.exists("./data")) {
-                create.dir("./data")
+                dir.create("./data")
                 message("./data directory created")
         }
         if (!dir.exists("./data/UCI HAR Dataset")) {
                 zipurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
                 datafile <- "./data/UCIHAR.zip"
-                unzip(datafile, exdir = "./data", overwrite = TRUE)
+                download.file(zipurl, datafile)
+                unzip(datafile, exdir = "./data")
                 message("UCI HAR Dataset imported")
         }
 }
