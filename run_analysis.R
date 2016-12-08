abe9095cf90b8a7150a7cd42d3f211877da2c067
Alex Robinson <alexrcapgemini@gmail.com>
2016-12-08 20:31:35 +0000

    Updated with heading and activity formatting

--- run_analysis.R
+++ run_analysis.R
@@ -1,29 +1,64 @@
 # Header information
-get_data <- function(){
-        if (!dir.exists("./data")) {
-                dir.create("./data")
-                message("./data directory created")
+
+get_data <- function(basedir){
+        if (!dir.exists(basedir)) {
+                dir.create(basedir)
+                message(paste(basedir, "directory created"))
         }
-        if (!dir.exists("./data/UCI HAR Dataset")) {
+        if (!dir.exists(paste0(basedir,"/UCI HAR Dataset"))) {
                 zipurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-                datafile <- "./data/UCIHAR.zip"
+                datafile <- paste0(basedir, "/UCIHAR.zip")
                 download.file(zipurl, datafile)
-                unzip(datafile, exdir = "./data")
+                unzip(datafile, exdir = basedir)
                 message("UCI HAR Dataset imported")
         }
-        datadir <<- "./data/UCI HAR Dataset/"
+        datadir <- paste0(basedir, "/UCI HAR Dataset/")
+        datadir
 }
 
-read_labels <- function(){
+read_labels <- function(datadir){
         labelfile <- paste0(datadir, "activity_labels.txt")
         featurefile <- paste0(datadir, "features.txt")
         
-        labelsframe <<- read.table(labelfile, col.names = c("key", "value"))
-        featuresframe <<- read.table(featurefile, col.names = c("key", "value"))
+        labelsframe <- read.table(labelfile, col.names = c("key", "value"))
+        featuresframe <- read.table(featurefile, col.names = c("key", "value"))
+        
+        output <- list(labelsframe, featuresframe)
+        output
         
 }
 
-read_test_set <- function(testset){
+extract_req_columns <- function(telemetry){
+        colpattern <- "(std|mean)"
+        output <- telemetry[, grep(colpattern, colnames(telemetry), value = TRUE)]
+        output
+        
+}
+
+format_heading <- function(colheading){
+        output <- tolower(colheading)
+        output <- gsub("\\.", "", output)
+        output
+}
+
+format_all_headings <- function(colheadings){
+        output <- sapply(colheadings, format_heading, simplify = TRUE, USE.NAMES = FALSE)
+        output
+}
+
+format_activity_label <- function(labelsframe, value_to_format){
+        output <- labelsframe[value_to_format,"value"]
+        output <- tolower(output)
+        output <- gsub("_", "", output)
+        output
+}
+
+format_all_activity_labels <- function(labelsframe, frame_to_format){
+        output <- sapply(frame_to_format, FUN = function(x){ y <- format_activity_label(labelsframe, x); y})
+        output
+}
+
+read_test_set <- function(datadir, featuresframe, labelsframe, testset){
         testsetdir <- paste0(datadir, testset, "/")
         if (!dir.exists(testsetdir)){
                 stop("No such test set")
@@ -36,14 +71,20 @@ read_test_set <- function(testset){
         subjectsframe <- read.table(subjectsfile, col.names = c("subject"))
         telemetryframe <- read.table(telemetryfile, col.names = featuresframe$value)
         
+        telemetryframe <- extract_req_columns(telemetryframe)
+        
+        colnames(telemetryframe) <- format_all_headings(colnames(telemetryframe))
+        
+        activitiesframe$activity <- format_all_activity_labels(labelsframe, activitiesframe$activity)
+        
         testsetframe <- data.frame(activitiesframe, subjectsframe, telemetryframe)
         
         testsetframe
 }
 
-collate_test_sets <- function(){
-        testframe <- read_test_set("test")
-        trainframe <- read_test_set("train")
+collate_test_sets <- function(datadir, featuresframe, labelsframe){
+        testframe <- read_test_set(datadir, featuresframe, labelsframe, "test")
+        trainframe <- read_test_set(datadir, featuresframe, labelsframe, "train")
 
         collatedframe <- merge(testframe, trainframe, all = TRUE)
 
@@ -52,10 +93,13 @@ collate_test_sets <- function(){
 }
 
 main <- function(){
-        get_data()
-        read_labels()
-        cl <- collate_test_sets()
+        basedir <- "./data"
+        datadir <- get_data(basedir)
+        labellist <- read_labels(datadir)
+        labelsframe <- labellist[[1]]
+        featuresframe <- labellist[[2]]
+        cl <- collate_test_sets(datadir, featuresframe, labelsframe)
         cl
 }
 
-main()
+dt <- main()
